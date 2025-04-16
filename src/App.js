2d6b902b1d63a46b3bd06be17b833f30af00a4c0
Wholesome-Gee <jiyong0419@naver.com>
2025-04-16 16:11:34 +0900

    #2.1 styled-components 설치 및 기본사용법

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import styled from "styled-components"; // npm i styled-components
+
+// styled-component 기본 선언 및 사용법
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.span`
+  color: white;
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <Father>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
+  );
 }
 
 export default App;
